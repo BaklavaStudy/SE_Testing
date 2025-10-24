ef5956bf77d4f613b75b51dd131c0446baa1f591
BaklavaStudy <s4005276@student.rmit.edu.au>
2025-10-24 20:22:54 +1100

    I hate configuration with  all of my heart <3

--- Jmeter-Selenium-Cucumber/Selenium/untitled/src/test/java/runners/testRunner.java
+++ Jmeter-Selenium-Cucumber/Selenium/untitled/src/test/java/runners/testRunner.java
@@ -1,14 +1,14 @@
-package runners;
-
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/.feature",
-        glue = "steps",
-)
-
-public class testRunner {
-}
+//package runners;
+//
+//import org.junit.runner.RunWith;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/resources/.feature",
+//        glue = "steps",
+//)
+//
+//public class testRunner {
+//}
--- Jmeter-Selenium-Cucumber/Selenium/untitled/src/test/java/steps/LoginSteps.java
+++ Jmeter-Selenium-Cucumber/Selenium/untitled/src/test/java/steps/LoginSteps.java
@@ -1,68 +1,68 @@
-package steps;
-
-
-import org.junit.jupiter.api.BeforeAll;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
-import java.util.Random;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class LoginSteps extends Hooks {
-    private String generatedEmail;
-
-    @Given(" I am on the DemoBlaze homepage")
-    public void i_am_on_the_DemoBlaze_homepage() {
-
-    }
-
-    (1) [INFO] Ineffective @Order annotation on method 'public void Category.phoneCategory() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Category', methodName = 'phoneCategory', methodParameterTypes = '']
-    at Category.phoneCategory(SourceFile:0)
-
-(2) [INFO] Ineffective @Order annotation on method 'public void Category.allCategories() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Category', methodName = 'allCategories', methodParameterTypes = '']
-    at Category.allCategories(SourceFile:0)
-
-(3) [INFO] Ineffective @Order annotation on method 'public void Category.monitorCategory() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Category', methodName = 'monitorCategory', methodParameterTypes = '']
-    at Category.monitorCategory(SourceFile:0)
-
-(4) [INFO] Ineffective @Order annotation on method 'public void Category.laptopCategoriesOG() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Category', methodName = 'laptopCategoriesOG', methodParameterTypes = '']
-    at Category.laptopCategoriesOG(SourceFile:0)
-
-(5) [INFO] Ineffective @Order annotation on method 'public void CartTest.placeOrderTest()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'CartTest', methodName = 'placeOrderTest', methodParameterTypes = '']
-    at CartTest.placeOrderTest(SourceFile:0)
-
-(6) [INFO] Ineffective @Order annotation on method 'public void CartTest.addItemToCart()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'CartTest', methodName = 'addItemToCart', methodParameterTypes = '']
-    at CartTest.addItemToCart(SourceFile:0)
-
-(7) [INFO] Ineffective @Order annotation on method 'public void ContactUs.testContactForm()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'ContactUs', methodName = 'testContactForm', methodParameterTypes = '']
-    at ContactUs.testContactForm(SourceFile:0)
-
-(8) [INFO] Ineffective @Order annotation on method 'public void ContactUs.testAboutUsVideo() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'ContactUs', methodName = 'testAboutUsVideo', methodParameterTypes = '']
-    at ContactUs.testAboutUsVideo(SourceFile:0)
-
-(9) [INFO] Ineffective @Order annotation on method 'public void Login_Signup.signup()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Login_Signup', methodName = 'signup', methodParameterTypes = '']
-    at Login_Signup.signup(SourceFile:0)
-
-(10) [INFO] Ineffective @Order annotation on method 'public void Login_Signup.login()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
-    Source: MethodSource [className = 'Login_Signup', methodName = 'login', methodParameterTypes = '']
-    at Login_Signup.login(SourceFile:0)
-//    When I; click the; "Log in" link
-//    And I; enter valid; "testuser" and "testpassword"
-//    And I; click the; "Log in" button
-//    Then I; should see; a welcome; message with; my username;
-
-}
+//package steps;
+//
+//
+//import org.junit.jupiter.api.BeforeAll;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.When;
+//import io.cucumber.java.en.Then;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import java.time.Duration;
+//import java.util.Random;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//public class LoginSteps extends Hooks {
+//    private String generatedEmail;
+//
+//    @Given(" I am on the DemoBlaze homepage")
+//    public void i_am_on_the_DemoBlaze_homepage() {
+//
+//    }
+//
+//    (1) [INFO] Ineffective @Order annotation on method 'public void Category.phoneCategory() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Category', methodName = 'phoneCategory', methodParameterTypes = '']
+//    at Category.phoneCategory(SourceFile:0)
+//
+//(2) [INFO] Ineffective @Order annotation on method 'public void Category.allCategories() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Category', methodName = 'allCategories', methodParameterTypes = '']
+//    at Category.allCategories(SourceFile:0)
+//
+//(3) [INFO] Ineffective @Order annotation on method 'public void Category.monitorCategory() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Category', methodName = 'monitorCategory', methodParameterTypes = '']
+//    at Category.monitorCategory(SourceFile:0)
+//
+//(4) [INFO] Ineffective @Order annotation on method 'public void Category.laptopCategoriesOG() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Category', methodName = 'laptopCategoriesOG', methodParameterTypes = '']
+//    at Category.laptopCategoriesOG(SourceFile:0)
+//
+//(5) [INFO] Ineffective @Order annotation on method 'public void CartTest.placeOrderTest()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'CartTest', methodName = 'placeOrderTest', methodParameterTypes = '']
+//    at CartTest.placeOrderTest(SourceFile:0)
+//
+//(6) [INFO] Ineffective @Order annotation on method 'public void CartTest.addItemToCart()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'CartTest', methodName = 'addItemToCart', methodParameterTypes = '']
+//    at CartTest.addItemToCart(SourceFile:0)
+//
+//(7) [INFO] Ineffective @Order annotation on method 'public void ContactUs.testContactForm()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'ContactUs', methodName = 'testContactForm', methodParameterTypes = '']
+//    at ContactUs.testContactForm(SourceFile:0)
+//
+//(8) [INFO] Ineffective @Order annotation on method 'public void ContactUs.testAboutUsVideo() throws java.lang.InterruptedException'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'ContactUs', methodName = 'testAboutUsVideo', methodParameterTypes = '']
+//    at ContactUs.testAboutUsVideo(SourceFile:0)
+//
+//(9) [INFO] Ineffective @Order annotation on method 'public void Login_Signup.signup()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Login_Signup', methodName = 'signup', methodParameterTypes = '']
+//    at Login_Signup.signup(SourceFile:0)
+//
+//(10) [INFO] Ineffective @Order annotation on method 'public void Login_Signup.login()'. It will not be applied because MethodOrderer.OrderAnnotation is not in use. Note that the annotation may be either directly present or meta-present on the method.
+//    Source: MethodSource [className = 'Login_Signup', methodName = 'login', methodParameterTypes = '']
+//    at Login_Signup.login(SourceFile:0)
+////    When I; click the; "Log in" link
+////    And I; enter valid; "testuser" and "testpassword"
+////    And I; click the; "Log in" button
+////    Then I; should see; a welcome; message with; my username;
+//
+//}
